d0229929323e1cc0eb21cd3beda9321a152631ff
NicolaiSzuwalski <132432642+NicolaiSzuwalski@users.noreply.github.com>
2023-05-15 09:53:35 +0200

    Update arrayFunctions.js

--- assets/js/opgaver/arrayFunctions.js
+++ assets/js/opgaver/arrayFunctions.js
@@ -9,11 +9,14 @@ let myPartArray = ['partElement1', 'partElement2', 'PartElement3'];
 // opgave 1 tilføj element4 til enden myArray
 
 // din kode her...
+myArray.push('element4')
 console.log(myArray);
 
 // opgave 1 fjern element4 igen
 
+
 // din kode her...
+myArray.pop('element4')
 console.log(myArray);
 
 //.........................................................
@@ -21,11 +24,13 @@ console.log(myArray);
 // opgave 2 tilføj element4 til starten myArray
 
 // din kode her...
+myArray.unshift('element4')
 console.log(myArray);
 
 // opgave 2 fjern element4 igen
 
 // din kode her...
+myArray.shift('element4')
 console.log(myArray);
 
 //.........................................................
@@ -58,7 +63,7 @@ træk blomkål og spidskål ud af arrayen myVegetables
 og ind i en ny array der hedder myCabbage*/
 
 // din kode her...
-
+let myCabbage = myVegetables.slice(1, 3)
 
 
 console.log(myCabbage);
@@ -70,7 +75,7 @@ console.log(myCabbage);
 
 
 // din kode her...
-
+let myYummies = myFruits.concat(myVegetables)
 
 
 console.log(myYummies);
@@ -83,6 +88,7 @@ console.log(myYummies);
 
 
 // din kode her...
+myYummies.sort()
 
 
 
@@ -94,6 +100,7 @@ console.log(myYummies);
 
 
 // din kode her...
+myYummies.reverse()
 
 console.log(myYummies);
 
@@ -107,7 +114,16 @@ console.log(myYummies);
 let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // din kode her...
+let myNextNumbers = myNumbers.map((x) =>{
+    return x+7;
+});
+
 console.log(myNextNumbers);
+
+let myLastNumbers = myNextNumbers.map((x) =>{
+    return x*12;
+});
+
 console.log(myLastNumbers);
 
 //.........................................................
@@ -115,6 +131,16 @@ console.log(myLastNumbers);
 skriv en kode der kan give os en ny array, udfra myNames, hvor navnet keld ikke findes i.
 */
 let myNames = ['keld', 'søren', 'jens', 'tine', 'keld', 'ib', 'lene', 'keld', 'frederikke', 'line', 'kurt'];
+
+const myNamesFilter = myNames.filter(removeKeld);
+
+function removeKeld(myNames){
+    if(myNames != 'keld'){
+        return myNames
+    }
+};
+
+console.log(myNamesFilter)
 
 
 //.........................................................
